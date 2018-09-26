0bbf0c4739ae0beed0ce7b1eb82cadc49ffc64cc
Jake Dillon <jakedillon@MacBook-Pro-60.local>
2018-09-26 13:28:19 -0500

    finished

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -24,11 +24,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
       
-        var randomNumber = Int(arc4random_uniform(100)) + 1
-        var UserTries = 5
-        var newGame = false
-        
-        
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         greetingLabel.text = "Guess number between 1-100"
@@ -38,72 +34,59 @@ class ViewController: UIViewController {
         guessedNumber.text = "1"
     }
 
+    var randomNumber = Int(arc4random_uniform(100)) + 1
+    var userTries = 5
+    var newGame = false
 
-    @IBAction func submitButtonTapped(_ sender: Any) {
-     let userInput = Int(numberSlider.value)
-
-        }
- 
     @IBAction func resetButton(_ sender: Any) {
-   
+        let newGame = true
+        if newGame == true {
+            userTries = 5
+            randomNumber = Int(arc4random_uniform(100)) + 1
+            resetButton.isHidden = false
+            viewDidLoad()
+        }
+        
+        
     }
+    
 
     @IBAction func numberSlider(_ sender: Any) {
         let numberGuessed = Int(numberSlider.value)
         guessedNumber.text = "\(numberGuessed)"
     }
-    if UserTries == 0 {
-    submitButton.isHidden = true
+    
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        let userInput = Int(numberSlider.value)
+    
+    
+    if userTries == 0 {
+    submitButton.isHidden = false
     resetButton.isHidden = false
-    greetingLabel.text = "you lost :weary:"
-    promptUser.text = """
+    greetingLabel.text = "you lost"
+    guessStatusLabel.text = """
     \(randomNumber) was the correct number
-    better luck next time
     """
-
+    
+} else if userInput == randomNumber {
+        guessStatusLabel.text = "you got it right"
+    
+    } else if userInput <= randomNumber {
+        guessStatusLabel.text = """
+    \(userInput) is too low
+    you have \(userTries) tries left
+    """
+    userTries -= 1
+    } else if userInput >= randomNumber {
+    guessStatusLabel.text = """
+    \(userInput) is too high
+    you have \(userTries) tries left
+    """
+    userTries -= 1
+    }
+    print("\(randomNumber) - \(userTries)")
 }
 
 
-    
-
-
-    
-
-
-        if UserTries == 0 {
-            userClicked.isHidden = true
-            restartButton.isHidden = false
-            topLabel.text = "you lost :weary:"
-            promptUser.text = """
-            \(randomNumber) was the correct number
-            better luck next time
-            """
-//        } else if userInput == randomNumber {
-//            promptUser.text = "you got it right"
-//
-//        } else if userInput <= randomNumber {
-//            promptUser.text = """
-//            \(userInput) is to low
-//            you have \(UserTries) tries left
-//            """
-//            UserTries -= 1
-//        } else if userInput >= randomNumber {
-//            promptUser.text = """
-//            \(userInput) is to high
-//            you have \(UserTries) tries left
-//            """
-//            UserTries -= 1
-//        }
-//        print("\(randomNumber) - \(UserTries)")
-//    }
-//
-//    @IBAction func userRestart(_ sender: Any) {
-//        newGame = true
-//        if newGame == true {
-//            UserTries = 5
-//            randomNumber = Int(arc4random_uniform(100)) + 1
-//            userClicked.isHidden = false
-//            viewDidLoad()
-//        }
-//    }
-//}
+}
